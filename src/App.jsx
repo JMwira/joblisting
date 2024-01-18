b884ba0d8fa2bc6937b707ca13a2951656756e91
JMwira <jaymwira@gmail.com>
2024-01-18 15:59:50 +0200

    final commit 01

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Jobs from './components/Jobs'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex flex-col h-full bg-secondary w-full' >
+        <div className='bg-slate-700 bg-opacity-20' >
+          <div className='lg:bg-bg_1 bg-bg_2 bg-cover flex p-4 w-full lg:bg-contain h-[200px]' >
+        </div>
+        <div className='flex bg-bg1' >
+          <Jobs/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./ index.html",
-    "./src/**/*.{js, ts, jsx, tsx}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "bg1":"hsl(180, 52%, 96%)",
+        "bg2":"hsl(180, 31%, 95%)",
+        "bg3":"#5D9F9D",
+        "bg4":"hsl(180, 14%, 20%)",
+        "pimary":"#5D9F9D",
+        "secondary":"#5DA5A3"
+      },
+      backgroundImage:{
+        'bg_1': "url('../src/assets/bg-header-desktop.svg')",
+        'bg_2': "url('../src/assets/bg-header-mobile.svg')",
+      }
+    },
   },
   plugins: [],
 }
